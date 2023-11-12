adaa255e88c6045ffdd02da1f3dba975d69ae3da
HeiderSati <37833734+HeiderSati@users.noreply.github.com>
2023-11-12 15:08:27 +0000

    Update GoProFileFix.sh

--- GoProFileFix.sh
+++ GoProFileFix.sh
@@ -5,7 +5,7 @@ for file in "$@"; do
 		filenameonly=$(basename "$file")
 		dironly=$(dirname "$file")
 
-		NewName="NEW_GH""${filenameonly:4:4}""${filenameonly:2:2}""${filenameonly:8:4}"
+		NewName="NEW_""${filenameonly:0:2}""${filenameonly:4:4}""${filenameonly:2:2}""${filenameonly:8:4}"
 		echo "Renaming File: " "$filenameonly" ", To: " "$NewName" 
 		mv "$dironly/$filenameonly" "$dironly/$NewName"
 
